5f7fcb064ad5e5e85728ab3300944ac5551ee720
Amol Vasaikar <amol.vasaikar1@gmail.com>
2023-07-12 12:08:05 +0530

    Initial Commit
    Initial Commit

---
+++ src/app/containers/default-layout/_nav.ts
@@ -0,0 +1,31 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    iconComponent: { name: 'cil-speedometer' },
+  },
+  {
+    name: 'Colors',
+    url: '/theme/colors',
+    iconComponent: { name: 'cil-drop' },
+  },
+  {
+    name: 'Typography',
+    url: '/theme/typography',
+    linkProps: { fragment: 'someAnchor' },
+    iconComponent: { name: 'cil-pencil' },
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    iconComponent: { name: 'cil-puzzle' },
+    children: [
+      {
+        name: 'Accordion',
+        url: '/base/accordion',
+      },
+    ],
+  },
+];
